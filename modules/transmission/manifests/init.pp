b8b1edeee737d9fb766cb6639d19408317644a2c
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2014-04-28 01:06:55 +0100

    Transmission download dirs

---
+++ modules/transmission/manifests/init.pp
@@ -0,0 +1,26 @@
+class transmission {
+  $config = hiera("transmission")
+
+  package { "transmission-daemon":
+    ensure => "latest",
+  }
+
+  file { "/etc/transmission-daemon/settings.json":
+    ensure => "file",
+    owner => "debian-transmission",
+    group => "debian-transmission",
+    mode => "0600",
+    content => template("transmission/settings.json.erb"),
+    require => Package["transmission-daemon"],
+  }
+
+  file {[
+    $config["download_dir"],
+    "${config['download_dir']}/incomplete"
+  ]:
+    ensure => "directory",
+    owner => "debian-transmission",
+    group => "staff",
+    mode => "0770",
+  }
+}
