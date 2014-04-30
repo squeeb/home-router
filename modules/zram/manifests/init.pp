ce20c141e704bd7a24ad5c271f3930e16572a5c6
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2014-04-30 01:17:08 +0100

    Check if virtual before trying zram

--- modules/zram/manifests/init.pp
+++ modules/zram/manifests/init.pp
@@ -1,28 +1,30 @@
 class zram {
-  file { "/etc/modprobe.d/zram.conf":
-    ensure => "file",
-    content => "options zram num_devices=${processorcount}",
-    owner => "root",
-    group => "root",
-    mode => "0664",
-    notify => Service["zram"],
-  }
+  if $is_virtual == false {
+    file { "/etc/modprobe.d/zram.conf":
+      ensure => "file",
+      content => "options zram num_devices=${processorcount}",
+      owner => "root",
+      group => "root",
+      mode => "0664",
+      notify => Service["zram"],
+    }
 
-  file { "/etc/init.d/zram":
-    ensure => "file",
-    source => "puppet:///modules/zram/zram.init",
-    owner => "root",
-    group => "root",
-    mode => "0755",
-    notify => Service["zram"],
-  }
+    file { "/etc/init.d/zram":
+      ensure => "file",
+      source => "puppet:///modules/zram/zram.init",
+      owner => "root",
+      group => "root",
+      mode => "0755",
+      notify => Service["zram"],
+    }
 
-  service { "zram":
-    ensure => "running",
-    enable => true,
-    require => File[
-      "/etc/modprobe.d/zram.conf",
-      "/etc/init.d/zram"
-    ],
+    service { "zram":
+      ensure => "running",
+      enable => true,
+      require => File[
+        "/etc/modprobe.d/zram.conf",
+        "/etc/init.d/zram"
+      ],
+    }
   }
 }
