c0ff42c8bddeb6d7e167eecc157559639a6f0c08
Chris Rigby <chris.rigby@onthebeach.co.uk>
2014-05-20 09:31:01 +0100

    Change cron

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -19,10 +19,18 @@ class puppet {
     require => File["/etc/puppet/puppet.conf"],
   }
 
-  cron { "puppet-run":
-    command => "/usr/local/bin/puppet-up && /usr/local/bin/puppet-run > /dev/null 2>&1",
+  cron { "puppet-up":
+    command => "/usr/local/bin/puppet-up > /dev/null 2>&1",
     hour    => "*",
-    minute  => "*/30",
+    minute  => "0",
+    require => [
+      Class["environment"],
+    ],
+  }
+  cron { "puppet-run":
+    command => "/usr/local/bin/puppet-run > /dev/null 2>&1",
+    hour    => "*",
+    minute  => "2",
     require => [
       Class["environment"],
       File["/etc/puppet/puppet.conf"],
