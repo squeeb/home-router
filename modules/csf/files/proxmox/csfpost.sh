138b24fbc0d83d63a9fa771e1400b439ddc44e6b
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2014-08-06 23:41:59 +0100

    Add nat rules for fun shit

--- modules/csf/files/proxmox/csfpost.sh
+++ modules/csf/files/proxmox/csfpost.sh
@@ -4,3 +4,5 @@
 /sbin/iptables -A FORWARD -j ACCEPT -p all -s 0/0 -i venet0
 /sbin/iptables -A FORWARD -j ACCEPT -p all -s 0/0 -o venet0
 /sbin/iptables -A INPUT -m addrtype --dst-type MULTICAST -j ACCEPT
+/sbin/iptables -t nat -A POSTROUTING -s '10.2.100.0/24' -o vmbr0 -j MASQUERADE
+/sbin/iptables -t nat -D POSTROUTING -s '10.2.100.0/24' -o vmbr0 -j MASQUERADE
