79d62485c8c3eb406196972167e243d685169ef5
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2014-08-25 15:49:24 +0100

    derp

--- modules/apache2/manifests/fastcgi.pp
+++ modules/apache2/manifests/fastcgi.pp
@@ -6,7 +6,7 @@ class apache2::fastcgi {
 
   exec { "a2enmod fastcgi":
     creates => "/etc/apache2/mods-enabled/fastcgi.load",
-    require => Package["libapache2-mod-fastcgi"],
+    require => Package["libapache2-mod-fcgid"],
     notify => Service["apache2"],
   }
 }
