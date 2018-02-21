94b44991242708387158e1efa1a9a3ab64c8528e
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2018-02-21 21:45:04 +0000

    Revert "Stupid ubuntu"
    This reverts commit 04fdd5182c6a406e0e73221a2a130c8a3695aef7.

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,22 +1,25 @@
 class sudo {
 
-  $defaults = hiera('sudo::defaults', [])
-  $command_aliases = hiera('sudo::command_aliases', [])
-  $groups = hiera('sudo::groups', [])
-  $users = hiera('sudo::users', [])
+  $defaults = hiera("sudo::defaults", [])
+  $command_aliases = hiera("sudo::command_aliases", [])
+  $groups = hiera("sudo::groups", [])
+  $users = hiera("sudo::users", [])
 
-  $sudoers = '/etc/sudoers'
-
-  package { 'sudo':
-    ensure => 'installed',
+  $sudoers = $::operatingsystem ? {
+    /^(Debian|Ubuntu)$/ => "/etc/sudoers.d/sudoers",
+    "CentOS" => "/etc/sudoers",
+    default => "/etc/sudoers",
+  }
+  package { "sudo":
+    ensure => "installed",
   }
 
   file { $sudoers:
-    ensure => 'present',
-    owner => 'root',
-    group => 'root',
-    mode => '0440',
-    content => template('sudo/sudoers.erb'),
-    require => Package['sudo'],
+    ensure => "present",
+    owner => "root",
+    group => "root",
+    mode => "0440",
+    content => template("sudo/sudoers.erb"),
+    require => Package["sudo"],
   }
 }
