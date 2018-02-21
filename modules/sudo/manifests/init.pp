04fdd5182c6a406e0e73221a2a130c8a3695aef7
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2018-02-21 21:42:38 +0000

    Stupid ubuntu

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,25 +1,22 @@
 class sudo {
 
-  $defaults = hiera("sudo::defaults", [])
-  $command_aliases = hiera("sudo::command_aliases", [])
-  $groups = hiera("sudo::groups", [])
-  $users = hiera("sudo::users", [])
+  $defaults = hiera('sudo::defaults', [])
+  $command_aliases = hiera('sudo::command_aliases', [])
+  $groups = hiera('sudo::groups', [])
+  $users = hiera('sudo::users', [])
 
-  $sudoers = $::operatingsystem ? {
-    /^(Debian|Ubuntu)$/ => "/etc/sudoers.d/sudoers",
-    "CentOS" => "/etc/sudoers",
-    default => "/etc/sudoers",
-  }
-  package { "sudo":
-    ensure => "installed",
+  $sudoers = '/etc/sudoers'
+
+  package { 'sudo':
+    ensure => 'installed',
   }
 
   file { $sudoers:
-    ensure => "present",
-    owner => "root",
-    group => "root",
-    mode => "0440",
-    content => template("sudo/sudoers.erb"),
-    require => Package["sudo"],
+    ensure => 'present',
+    owner => 'root',
+    group => 'root',
+    mode => '0440',
+    content => template('sudo/sudoers.erb'),
+    require => Package['sudo'],
   }
 }
