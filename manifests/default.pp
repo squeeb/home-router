67ecffe0378164b497adfaeb4259116366d7326a
Chris Rigby <squeeb@radioglitch.com>
2018-10-27 19:25:20 +0100

    rrr

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,8 @@
 hiera_include("classes", [])
-Exec { path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" }
+Exec {
+  path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
 File { backup => false }
 
 schedule { "bidaily":
